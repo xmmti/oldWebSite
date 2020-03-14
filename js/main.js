00797ca2e6d4f90aaf1ce86a10d9668972904f05
X7md <54203033+X7md@users.noreply.github.com>
2020-03-14 15:29:08 +0300

    New WebSite and Design

---
+++ js/main.js
@@ -0,0 +1,41 @@
+var mode_is = 0;
+AOS.init();
+var hour = new Date().getHours();
+  if (hour > 12) {
+    Set_Dark()
+    mode_is = 0
+  } else {
+    Set_light()
+    mode_is = 1
+  };
+
+$( "#bt-color" ).click(function() {
+  $("div, nav").fadeOut(100).fadeIn(65); 
+if (mode_is == 0) {
+  Set_light()
+	mode_is = 1
+} else {
+  Set_Dark()
+	 mode_is = 0
+}
+});
+
+function Set_light() {
+  $(".cardmode").css("background-color","#fff");
+  $(".cardmode").css("color","#000000");
+  $("body").css("background-color","#fff");
+   //$('#bt-color').text("Dark");
+   
+    $('#bt-color').addClass('btn-dark').removeClass('bg-light');
+	$('nav').css("background-color","#fff");
+	$('nav, #text_is').css("color","#212529");
+}
+function Set_Dark() {
+ $(".cardmode").css("background-color","#343a40");
+  $(".cardmode").css("color","#fff");
+  $("body").css("background-color","#202020");
+  //$('#bt-color').text("Light");
+  $('#bt-color').addClass('bg-light').removeClass('btn-dark');
+  $('nav').css("background-color","#202020");
+   $('nav, #text_is').css("color","#fff");
+}
