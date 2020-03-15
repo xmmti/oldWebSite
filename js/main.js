3304fc1961c5810b1ae1b12b74993ae18d887bc8
X7md <54203033+X7md@users.noreply.github.com>
2020-03-15 21:52:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,6 +20,20 @@ if (mode_is == 0) {
 }
 });
 
+  this.addEventListener('activate', function(event) {
+  var cachesToKeep = ['v2'];
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cachesToKeep.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
 function Set_light() {
   $(".cardmode").css("background-color","#f3f3f3");
   $(".cardmode").css("color","#000000");
